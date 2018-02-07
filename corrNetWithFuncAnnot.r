b5d26acd31ad96e14d16964c46a1ebb68ff7f5e1
kimcdata <35961519+kimcdata@users.noreply.github.com>
2018-02-07 23:33:31 +0000

    added rownames to path tables

--- corrNetWithFuncAnnot.r
+++ corrNetWithFuncAnnot.r
@@ -240,11 +240,11 @@ corrNetWithFuncAnnot = function(gene_expression_file, target_file, target_pathwa
 
 	for(p in names(path_tables)){
 
-		to_write = path_tables[[p]]
+		to_write = t(path_tables[[p]])
 		rownames(to_write) = target_genes_harm
 		str(to_write)
 
-		write.table(t(to_write), file = paste0(results_dir,"/Target.pathway.",p,".enrichment.scores.txt"), quote=F, sep = "\t", col.names=NA)
+		write.table(to_write, file = paste0(results_dir,"/Target.pathway.",p,".enrichment.scores.txt"), quote=F, sep = "\t", col.names=NA)
 
 	}
 	
